e7221e00a1f726129c5da7739fd42a28f4c7a1dc
vm <vm@email.com>
2023-08-03 08:57:27 +0700

    a

--- src/Infrastructure/Data/DashboardContext.cs
+++ src/Infrastructure/Data/DashboardContext.cs
@@ -27,7 +27,7 @@ public partial class DashboardContext : DbContext, IDashboardContext
 
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=dashboard;User Id=app_dashboard;Password=dfikhDy&$%1!!");
+//        => optionsBuilder.UseNpgsql("Server=127.0.0.1;Port=5432;Database=");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
