2132bbde939d2446cd4242a22af40173ee4cea12
ronny <aaa@aaa.com>
2023-08-15 15:01:42 +0700

    text editor

--- src/Web/ClientApp/src/pages/home/Home.tsx
+++ src/Web/ClientApp/src/pages/home/Home.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import BookmarkBox from "../../components/bookmarkBox/BookmarkBox";
 import TextEditor from "../../components/textEditor/TextEditor";
 import "./home.scss";
 
 const Home = () => {
+
+  const [state, setState] = useState({ notes: [
+    {title: "first note", content: "first content"},
+    {title: "second note", content: "second content"}
+  ] })
+
   return (
     <div className="home">
       <div className="box-1">
         <BookmarkBox />
       </div>
-      <TextEditor data={{ value: 'wawax' }}/>
+      {state.notes.map((item) => <TextEditor {...item}/>)}      
     </div>
   );
 };
