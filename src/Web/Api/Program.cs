8dda0197cebf81f3e5227601f01ce1f0c4871d7c
vm <vm@email.com>
2023-09-01 19:46:51 +0700

    Remove cors

--- src/Web/Api/Program.cs
+++ src/Web/Api/Program.cs
@@ -12,21 +12,7 @@ ConfigureFastEndpoints();
 
 void ConfigureFastEndpoints()
 {
-    var AllowSpecificOriginsId = "AllowSpecificOriginsId";
     var builder = WebApplication.CreateBuilder(args);
-
-    builder.Services.AddCors(options =>
-    {
-        options.AddPolicy(name: AllowSpecificOriginsId,
-                          policy =>
-                          {
-                              string[]? origins = builder.Configuration.GetSection("CorsOrigins").Get<string[]>();
-                              if (origins != null)
-                              {
-                                  policy.WithOrigins(origins);
-                              }
-                          });
-    });
 
     builder.Logging.ClearProviders();
 
@@ -48,7 +34,6 @@ void ConfigureFastEndpoints()
 
     WebApplication app = builder.Build();
 
-    app.UseCors(AllowSpecificOriginsId);
     app.UseMiddleware<ExceptionHandlerMiddleware>();
     app.UseFastEndpoints(options => {
         options.Endpoints.RoutePrefix = "api";
