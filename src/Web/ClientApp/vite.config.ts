e6f11cf551cd9840e9938662ea9f83e836a6dc2d
X123A <X123A@gmail.com>
2024-09-30 16:43:43 +0700

    update proxy url

--- src/Web/ClientApp/vite.config.ts
+++ src/Web/ClientApp/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:5000/",
+                target: "https://localhost:7777/",
                 changeOrigin: true,
                 //rewrite: (path) => path.replace(/\/dashboard/, ''),
                 secure: false
